acc65992f68d5e64ca32a93404ab0b93e9aa6d67
vkasraj <vikasraj1911@gmail.com>
2019-09-20 18:48:06 +0530

    user: user routes final

--- app/user/user.routes.ts
+++ app/user/user.routes.ts
@@ -1,18 +1,48 @@
-import { FastifyRequest } from "fastify";
+import { ServerResponse } from "http";
+import { FastifyRequest, FastifyReply } from "fastify";
 import { RegisterRoute } from "../../@types/types";
 import { UserDAL } from "./user.dal";
 
 export const userRoutes: RegisterRoute = async fastify => {
-    fastify.get("/:userID", async (req: FastifyRequest) => {
-        const { userID } = req.params;
+    fastify.get(
+        "/:userID",
+        async (req: FastifyRequest, res: FastifyReply<ServerResponse>) => {
+            const { userID } = req.params;
 
-        return new UserDAL({
-            _id: userID,
-        }).findOne();
-    });
+            if (!userID) {
+                res.code(406);
+
+                return {
+                    message: "Please provide a valid user id",
+                };
+            }
+
+            const user = await new UserDAL({
+                _id: userID,
+            }).findOne();
+
+            if (!user) {
+                res.code(409);
+
+                return {
+                    message: "User not found with specified id :/",
+                };
+            }
+
+            return {
+                message: "Successful",
+                user,
+            };
+        }
+    );
 
     fastify.get("/", async () => {
-        return new UserDAL().findAll();
+        const users = await new UserDAL().findAll();
+
+        return {
+            message: "Successful",
+            users,
+        };
     });
 
     return true;
