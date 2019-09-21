bc080cd58f104ace211310bf87065fca3d588ea7
vkasraj <vikasraj1911@gmail.com>
2019-09-21 11:33:34 +0530

    package: removed unused dependencies

--- config/keys.ts
+++ config/keys.ts
@@ -1,3 +1,3 @@
-export const { PORT, NODE_ENV, MONGO_URI, TOKEN_KEY, TOKEN_EXP } = process.env;
+export const { PORT, NODE_ENV, MONGO_URI } = process.env;
 
 export const isDev = NODE_ENV !== "production";
